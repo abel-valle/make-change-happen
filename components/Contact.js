4bb7d5e96066b970d0fa5a22abed60a266eef604
abel-valle <a.valle.chavez@gmail.com>
2023-09-11 17:52:55 -0600

    Add of styled Contact form

--- components/Contact.js
+++ components/Contact.js
@@ -27,48 +27,76 @@ const Contact = () => {
 
   return (
     <div
-      className="max-w-screen-xl mt-8 mb-6 sm:mt-14 sm:mb-14 px-6 sm:px-8 lg:px-16 mx-auto"
+      className="bg-gradient-to-b from-white-300 to-white-500 w-full py-14"
       id="contact"
     >
-      <ScrollAnimationWrapper className="relative w-full mt-16">
-        <motion.div
-          variants={scrollAnimation}
-          custom={{ duration: 3 }}
-        >
-          <div className="bg-white-500">
-            <form onSubmit={submitData}>
-              <h1>Contacto</h1>
-              <input
-                autoFocus
-                onChange={(e) => setName(e.target.value)}
-                placeholder="Nombre"
-                type="text"
-                value={name}
-              />
-              <input
-                onChange={(e) => setEmail(e.target.value)}
-                placeholder="e-mail"
-                type="email"
-                value={email}
-              />
-              <textarea
-                cols={50}
-                onChange={(e) => setSubject(e.target.value)}
-                placeholder="Asunto"
-                rows={3}
-                value={subject}
-              />
-              <input
-                onChange={(e) => setPhone(e.target.value)}
-                placeholder="Telefono"
-                type="tel"
-                value={phone}
-              />
-              <input disabled={!name || !email} type="submit" value="Enviar" />
-            </form>
-          </div>
-        </motion.div>
-      </ScrollAnimationWrapper>
+      <div className="max-w-screen-xl px-6 sm:px-8 lg:px-16 mx-auto flex flex-col w-full text-center justify-center">
+        <center>
+          <ScrollAnimationWrapper>
+            <motion.div variants={scrollAnimation} custom={{ duration: 3 }}>
+              <div class="block max-w-md rounded-lg bg-white p-6 shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-teal-100">
+                <h1 className="text-1xl lg:text-2xl xl:text-3xl font-medium text-teal-500 mb-6">
+                  ¡Contáctanos!
+                </h1>
+                <form>
+                  <div class="relative mb-6" data-te-input-wrapper-init>
+                    <input
+                      autoFocus
+                      type="text"
+                      class="peer block min-h-[auto] w-full rounded border-0 px-3 py-[0.30rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-100"
+                      id="exampleInput7"
+                      placeholder="Nombre (cómo te gusta que te digan)"
+                      value={name}
+                      onChange={(e) => setName(e.target.value)}
+                    />
+                  </div>
+
+                  <div class="relative mb-6" data-te-input-wrapper-init>
+                    <input
+                      type="email"
+                      class="peer block min-h-[auto] w-full rounded border-0 px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-100"
+                      id="exampleInput8"
+                      placeholder="Correo electrónico"
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                    />
+                  </div>
+
+                  <div class="relative mb-6" data-te-input-wrapper-init>
+                    <input
+                      type="tel"
+                      class="peer block min-h-[auto] w-full rounded border-0 px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-100"
+                      id="exampleInput9"
+                      placeholder="Número de Whatsapp para contactarte"
+                      value={phone}
+                      onChange={(e) => setPhone(e.target.value)}
+                    />
+                  </div>
+
+                  <div class="relative mb-6" data-te-input-wrapper-init>
+                    <textarea
+                      class="peer block min-h-[auto] w-full rounded border-0 px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-100"
+                      id="exampleFormControlTextarea13"
+                      rows="3"
+                      placeholder="¿En qué podemos ayudarte?"
+                      onChange={(e) => setSubject(e.target.value)}
+                    ></textarea>
+                  </div>
+
+                  <button
+                    type="submit"
+                    class="text-white-500 bg-yellow-500 hover:bg-teal-500 dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]] inline-block w-full rounded bg-primary px-6 pb-2 pt-2.5 text-sm font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
+                    data-te-ripple-init
+                    data-te-ripple-color="light"
+                  >
+                    Enviar
+                  </button>
+                </form>
+              </div>
+            </motion.div>
+          </ScrollAnimationWrapper>
+        </center>
+      </div>
     </div>
   );
 };
